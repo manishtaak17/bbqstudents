b8b139a625bdaf50571b8f3c2ad1204b8d343641
radhe142357 <radhakrishan.mdb@gmail.com>
2025-07-25 18:06:29 +0530

    chasgs

---
+++ src/app/investor/ResourcesSection.tsx
@@ -0,0 +1,88 @@
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+
+  
+const ResourcesSection = () => {
+    const [showLastDiv, setShowLastDiv] = useState(false);
+    const lastDivRef = useRef(null);
+    const [lastDivHeight, setLastDivHeight] = useState(0);
+
+    useEffect(() => {
+        if (lastDivRef.current) {
+            setLastDivHeight(lastDivRef.current.scrollHeight);
+        }
+    }, []);
+
+    const toggleLastDiv = () => {
+        setShowLastDiv(!showLastDiv);
+    };
+
+    return (
+            <div className="max-w-[1262px] m-auto px-5 pt-[40px] md:pt-[80px] pb-[50px] md:pb-[175px]">
+                <div className="">
+                    <h5 className="text-darkpink font-firaSans-condensed font-semibold italic text-2xl md:text-3xl leading-[40px] tracking-[-1%] uppercase flex items-center gap-2 ">
+                        <span className="block size-2 md:size-3 bg-redbrown animate-dotPulse"></span>RESOURCES</h5>
+                    <p className="text-darkgray font-firaSans-condensed font-normal text-2xl leading-[130%] mt-5 ml-0 md:ml-5 max-w-[100%] lg:max-w-[296px] mb-[60px]">For any specific queries, reach out to our team members.</p>
+                </div>
+                <div className="ml-0 md:ml-5 flex justify-center md:justify-between gap-7 md:gap-5 flex-wrap lg:flex-nowrap">
+                    <div className="flex flex-row md:flex-col justify-between w-full md:w-[48%] gap-5 md:gap-0 border-t md:border-0 border-darkpink pt-4 md:pt-0" >
+                        <h5 className="border-0 md:border-t border-darkpink w-full max-w-[50%] md:max-w-[100%] lg:max-w-[295px] text-darkgray font-firaSans-condensed font-medium text-lg md:text-2xl leading-[28px] md:leading-[30px]  pt-5 ">For Shareholders' Grievance Redressal</h5>
+                        <div className="w-full max-w-[100%]">
+                            <div className="mb-6 md:mb-8 max-w-[100%] lg:max-w-[295px]">
+                                <h5 className="text-darkgray font-firaSans-condensed font-medium md:font-normal text-lg md:text-2xl leading-[23px] md:leading-[30px] md:mt-8 mb-5 md:mb-0 ">Ms. Nagamani CY</h5>
+                                <p className="text-darkgray font-firaSans-condensed font-normal text-sm md:text-lg leading-[17px] md:leading-[27px] ">Company Secretary & Compliance Officer</p>
+                            </div>
+                            <p className="text-darkgray font-firaSans-condensed font-normal text-sm md:text-lg leading-[17px] md:leading-[27px] flex gap-1 flex-wrap">
+                                <b>Email:</b><a href="mailto:compliance@barbequenation.com" className="break-all">compliance@barbequenation.com</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex flex-row md:flex-col justify-between w-full md:w-[48%] gap-5 md:gap-0 border-t md:border-0 border-darkpink pt-4 md:pt-0" >
+                        <h5 className="border-0 md:border-t border-darkpink w-full max-w-[50%] md:max-w-[100%] lg:max-w-[295px] text-darkgray font-firaSans-condensed font-medium text-lg md:text-2xl leading-[28px] md:leading-[30px]  pt-5 ">For Shareholders' Grievance Redressal</h5>
+                        <div className="w-full max-w-[100%]">
+                            <div className="mb-6 md:mb-8 max-w-[100%] lg:max-w-[295px]">
+                                <h5 className="text-darkgray font-firaSans-condensed font-medium md:font-normal text-lg md:text-2xl leading-[23px] md:leading-[30px] md:mt-8 mb-5 md:mb-0 ">Ms. Nagamani CY</h5>
+                                <p className="text-darkgray font-firaSans-condensed font-normal text-sm md:text-lg leading-[17px] md:leading-[27px] ">Company Secretary & Compliance Officer</p>
+                            </div>
+                            <p className="text-darkgray font-firaSans-condensed font-normal text-sm md:text-lg leading-[17px] md:leading-[27px] flex gap-1 flex-wrap">
+                                <b>Email:</b><a href="mailto:compliance@barbequenation.com" className="break-all">compliance@barbequenation.com</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        ref={lastDivRef}
+                        className={`flex flex-row md:flex-col justify-between w-full md:w-[48%] gap-5 md:gap-0 border-t md:border-0 border-darkpink pt-4 md:pt-0 transition-all duration-300 ease-in-out transform ${showLastDiv
+                            ? 'max-h-[1000px] opacity-100 translate-y-0 mb-8'
+                            : 'max-h-0 opacity-0 -translate-y-4 overflow-hidden md:max-h-none md:opacity-100 md:translate-y-0 mb-0'
+                            }`}
+                    >
+                        <h5 className="border-0 md:border-t border-darkpink w-full max-w-[50%] md:max-w-[100%] lg:max-w-[295px] text-darkgray font-firaSans-condensed font-medium text-lg md:text-2xl leading-[28px] md:leading-[30px]  pt-5 ">For Shareholders' Grievance Redressal</h5>
+                        <div className="w-full max-w-[100%]">
+                            <div className="mb-6 md:mb-8 max-w-[100%] lg:max-w-[295px]">
+                                <h5 className="text-darkgray font-firaSans-condensed font-medium md:font-normal text-lg md:text-2xl leading-[23px] md:leading-[30px] md:mt-8 mb-5 md:mb-0 ">Ms. Nagamani CY</h5>
+                                <p className="text-darkgray font-firaSans-condensed font-normal text-sm md:text-lg leading-[17px] md:leading-[27px] ">Company Secretary & Compliance Officer</p>
+                            </div>
+                            <p className="text-darkgray font-firaSans-condensed font-normal text-sm md:text-lg leading-[17px] md:leading-[27px] flex gap-1 flex-wrap">
+                                <b>Email:</b><a href="mailto:compliance@barbequenation.com" className="break-all">compliance@barbequenation.com</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <button
+                    className="flex md:hidden items-center gap-4 text-darkgray font-fira-sans-condensed font-semibold text-lg leading-[30px] m-auto mt-[35px]"
+                    onClick={toggleLastDiv}
+                >
+                    {showLastDiv ? 'LESS' : 'MORE'}
+                    <Image
+                        src={'/icons/arrowdown.png'}
+                        alt="icon"
+                        width={15}
+                        height={7}
+                        className={`transition-transform duration-300 ${showLastDiv ? 'rotate-180' : 'rotate-0'}`}
+                    />
+                </button>
+            </div>
+    );
+};
+
+export default ResourcesSection
---
+++ src/app/investor/StockExchangeSection.tsx
@@ -0,0 +1,85 @@
+"use client"
+import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+
+
+
+const StockExchangeSection = () => {
+  const [stocks, setStocks] = useState([]);
+
+  // Mock API call to fetch stock data
+  useEffect(() => {
+    const fetchStockData = async () => {
+      // Simulated API response
+      const mockData = [
+        {
+          id: 1,
+          exchange: 'BSE',
+          value: '3,500',
+          change: '−6.75 (1.76%)',
+        },
+        {
+          id: 2,
+          exchange: 'NSE',
+          value: '3,520',
+          change: '−5.50 (1.50%)',
+        },
+      ];
+      setStocks(mockData);
+    };
+
+    fetchStockData();
+  }, []);
+
+  return (
+      <div className="max-w-[1262px] mx-auto px-5">
+        <div className="flex justify-between gap-0 md:gap-0 flex-wrap md:flex-nowrap">
+          <div className="w-full max-w-[100%] md:max-w-[290px] flex items-center lg:items-end border-b md:border-b-0 border-darkgray pb-5 md:pb-0">
+            <h5 className="text-darkpink font-firaSans-condensed font-semibold italic text-2xl md:text-3xl leading-[33px] md:leading-[40px] tracking-[-1%] uppercase flex items-start md:items-center gap-4 md:gap-2 max-w-[301px]">
+              <p className="block size-3 bg-redbrown mt-2 md:mt-0"></p>
+              <span>
+                Stock exchange <br /> information
+              </span>
+            </h5>
+          </div>
+          <div className="w-full max-w-[100%] md:max-w-[747px] flex justify-between flex-wrap md:flex-nowrap gap-5 mt-[66px] md:mt-0 px-4 md:px-0">
+            {stocks.map((stock) => (
+              <div
+                key={stock.id}
+                className="w-full max-w-[100%] md:max-w-[292px]"
+              >
+                <div className="border-b-0 md:border-b border-darkgray pb-0 md:pb-5 mb-5">
+                  <p className="text-darkgray font-firaSans-condensed font-normal md:font-semibold not-italic md:italic text-2xl md:text-3xl leading-[40px] tracking-[-1%] uppercase flex justify-between md:justify-start items-center gap-5">
+                    {stock.exchange}{' '}
+                    <Image
+                      src="/icons/areo-45.svg"
+                      alt="icon"
+                      width={20}
+                      height={20}
+                    />
+                  </p>
+                </div>
+                <div className="border-b md:border-b-0 border-darkgray pb-4 md:pb-0">
+                  <h5 className="flex justify-between items-center gap-5 text-bgbrown font-firaSans-condensed italic md:not-italic font-semibold text-[30px] md:text-[46px] leading-[40px] md:leading-[56px]">
+                    {stock.value}{' '}
+                    <Image
+                      src="/icons/stock.svg"
+                      className="mb-[-19px] md:mb-0"
+                      alt="icon"
+                      width={62}
+                      height={35}
+                    />
+                  </h5>
+                  <p className="text-bgbrown font-firaSans-condensed font-normal md:font-medium text-[22px] md:text-2xl leading-[27px] md:leading-[56px]">
+                    {stock.change} today
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default StockExchangeSection;
